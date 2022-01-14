b78e4a3103650735770b056ded92aa0de468e411
Aaron Gullickson <aarongullickson@gmail.com>
2022-01-14 10:33:40 -0800

    Remove out of date code snippet

--- read_sucs_data.R
+++
@@ -1,5 +0,0 @@
-
-source("check_packages.R")
-
-sucs <- read_sheet("https://docs.google.com/spreadsheets/d/1x9bvFqSb4_or8JbvGj2LnezGkChWxEzRPf5FXvjonHE",
-                   sheet="Systems", skip=1)
